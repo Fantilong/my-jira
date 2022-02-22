577182c74de32421aaa43cc34234fc0f60149845
fantl <fantl@shanhs.com>
2022-02-22 21:01:38 +0800

    chore: 给 userDebounce 添加泛型

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-export const isFalsy = (value: any) => (value === 0 ? true : !!value);
+export const isFalsy = (value: unknown) => (value === 0 ? true : !!value);
 
 export const cleanObject = (obj: Object) => {
   const result = JSON.parse(JSON.stringify(obj));
@@ -21,8 +21,10 @@ export const useMount = (callback: () => void) => {
   }, []);
 };
 
-export const useDebounce = (value: any, delay?: number) => {
+export const useDebounce = <V>(value: V, delay?: number) => {
   const [debounceValue, setDebounceValue] = useState(value);
+
+  // console.log(value.mayNotExist);
 
   useEffect(() => {
     // 当 param 变化后，才设置定时器，
