64db847d578f09a9764aee40c9541b9b507e1540
fantl <fantl@shanhs.com>
2022-03-05 14:53:03 +0800

    chore: login组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
 import { ProjectListScreen } from "./screens/project-list";
-import { TsReacTTest } from "./screens/project-list/try-use-array";
+import { LoginScreen } from "./screens/login";
+// import { TsReacTTest } from "./screens/project-list/try-use-array";
 
 function App() {
   return (
     <div className="App">
       {/* <ProjectListScreen /> */}
-      <TsReacTTest />
+      {/* <TsReacTTest /> */}
+      <LoginScreen />
     </div>
   );
 }
