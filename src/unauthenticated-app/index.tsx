f82c1282cdc26d5e91a1bad120ab07434b6d0a39
Fantilong <fantl@shanhs.com>
2022-03-14 16:49:06 +0800

    chore: 日常

--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -19,7 +19,7 @@ export const UnauthencatedApp = () => {
         <Title>{isRegister ? "请注册" : "请登录"}</Title>
         {isRegister ? <RegisterScreen /> : <LoginScreen />}
         <Divider />
-        <Button type="text" onClick={() => setIsRegister(!isRegister)}>
+        <Button type="link" onClick={() => setIsRegister(!isRegister)}>
           {isRegister ? "已经有账号了？直接登录" : "没有账号？注册新账号"}
         </Button>
       </ShadowCard>
